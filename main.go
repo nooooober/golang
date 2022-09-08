93dab6a777403193cc43cf0ac01a2fefc641e348
tsf <tt@.com>
2022-09-08 18:01:23 +0800

    last commmit

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"fiber_kafka/router"
-
-	"github.com/gofiber/fiber/v2"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
 )
 
+type User struct {
+	Name   string   `json:"name"`
+	Nge    int      `json:"age"`
+	Friend []string `json:"friend"`
+}
+
 func main() {
-	app := fiber.New()
-	router.Router(app)
-	app.Listen(":3000")
+	usr := User{"Bob", 18, []string{"Tom", "Jim"}}
+	b, _ := json.Marshal(usr)
+	fmt.Println(string(b))
+	var nux interface{}
+	json.Unmarshal(b, &nux)
+	fmt.Println(nux)
+	fmt.Println(base64.StdEncoding.EncodeToString([]byte("fdjfasdjfjksda")))
+	fmt.Println()
 }
